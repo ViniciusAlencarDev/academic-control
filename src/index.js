83c12deeea215d06351168aca444ad4b683237fa
Vinícius Alencar <viniciusalencardev@gmail.com>
2020-06-11 17:54:54 -0300

    Funcionalidades finalizadas

--- src/index.js
+++ src/index.js
@@ -192,7 +192,7 @@ app.get('/marcacao/:id_frequencia', async (req, res) => {
 //     secure: false,
 //     auth: {
 //         user: 'professorteste@yahoo.com',
-//         pass: '123456sete'
+//         pass: 'mfvvgoyvbhnvoeme'
 //     }
 // });
 
@@ -201,8 +201,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: '@gmail.com',
-        pass: ''
+        user: 'academiccontrol2020@gmail.com',
+        pass: '123456sete'
     },
 });
 
@@ -213,25 +213,33 @@ app.post('/recuperarsenha', async (req, res) => {
         data: []
     }
 
-    const codigo = crypto.createHmac('sha256', '123456')
-        .update(email)
-        .digest('hex');
+    const verificar = await connection('professores')
+        .select('*')
+        .where('EMAIL', email)
+    
+    if(verificar.length) {
+        // const codigo = crypto.createHmac('sha256', '123456')
+        //     .update(email)
+        //     .digest('hex');
+        let buff = new Buffer(email);
+        const codigo = buff.toString('base64');
 
-    let link = `${protocol}://${ip}:${port}/verificarcodigo.html?codigo=${codigo}`;
-    console.log(link)
+        let link = `${protocol}://${ip}:${port}/verificarcodigo.html?codigo=${codigo}`;
+        // console.log(link)
 
-    // await transporter.sendMail({
-    //     from: 'Academic Control',
-    //     to: email,
-    //     subject: 'Recuperação de senha',
-    //     html: `<h1>Olá</h1>
-    //         para recuperar sua senha <a href="${link}" target="_blank">clique aqui</a>
-    //         <h4>Caso não solicitou recuperação de senha favor desconsiderar</h4>`,
-    //     text: ``,
-    // }).then(res => {
-    //     response.success = true;
-    // }).catch(erro => console.log(erro))
-    response.success = true;
+        await transporter.sendMail({
+            from: 'Academic Control',
+            to: email,
+            subject: 'Recuperação de senha',
+            html: `<h1>Olá</h1>
+                para recuperar sua senha <a href="${link}" target="_blank">clique aqui</a>
+                <h4>Caso não solicitou recuperação de senha favor desconsiderar</h4>`,
+            text: ``,
+        }).then(res => {
+            response.success = true;
+        }).catch(erro => console.log(erro))
+    } else
+        response.data = ["Email nao cadastrado"]
     
     res.send(response)
 })
@@ -244,11 +252,16 @@ app.post('/verificarcodigo', async (req, res) => {
     }
 
     const { email, senha } = req.body;
-    console.log(email)
-    
 
-    // const data = await connecion('professores')
-    //     .update('senha', senha)
+    let buff = new Buffer(`${email}=`, 'base64');
+    let emailDecoded = buff.toString('ascii');
+
+    const data = await connection('professores')
+        .update('SENHA', senha)
+        .where('EMAIL', emailDecoded)
+
+    if(data > 0)
+        response.success = true;
 
     res.send(response);
 
